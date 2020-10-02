dfdabc32fae791af013c2d4a8a6f86bfa446e4a7
thomasindrias <thoin216@student.liu.se>
2020-10-02 12:54:28 +0200

    telestrations test

--- demo/telestrations.ai/gameState.js
+++ demo/telestrations.ai/gameState.js
@@ -1,5 +1,150 @@
+class Player {
+  constructor(name, bot = false) {
+    this.bot = bot;
+    this.name = name;
+    this.img = null;
+    this.keyword = null;
+  }
+}
 class GameState {
-    constructor(players){
-        this.players = players;
+  constructor(players) {
+    this.players = players;
+    this.history = [];
+    this.playerIndex = 0;
+    this.state = 0;
+    this.stateID = [
+      "draw-state",
+      "ai-state",
+      "guess-state",
+      "end-state",
+    ];
+  }
+
+  showPlayerState() {
+    document.getElementById("playerIndex").innerText = this.players[this.playerIndex].name;
+    document
+      .getElementById("display-player-state")
+      .style.setProperty("display", "block", "important");
+  }
+
+  drawState() {
+    clearCanvas();
+    this.showPlayerState();
+
+    if (this.playerIndex === 0) {
+      // If the first player, take a random keyword to draw
+      const random = Math.floor(Math.random() * categories.length);
+      this.players[this.playerIndex].keyword = categories[random].split('_').join(' ');
+    } else {
+      // Get previous player's keyword.
+      this.players[this.playerIndex - 1].keyword = document.getElementById("input-guess").value;
+
+      // Push previous player into history
+      this.history.push(this.players[this.playerIndex - 1]);
+
+      // Take the keyword from the latest player (previous player)
+      this.players[this.playerIndex].keyword = this.history[this.history.length - 1].keyword;
     }
+
+    document.getElementById("draw-keyword").innerText = this.players[this.playerIndex].keyword;
+
+    this.show("draw-state");
+  }
+
+  AIState() {
+    this.show("ai-state");
+    var bot = new Player("AI", true);
+
+    // Save canvas as image for previous player (done here so the player get's to finish drawing)
+    // (img src type)
+    this.players[this.playerIndex - 1].img = saveCanvas();
+    this.history.push(this.players[this.playerIndex - 1]);
+
+    // Get prediction and save as a keyword for the bot
+    bot.keyword = getFrame();
+
+    // If player draws so bad so AI cant predict, continue with a random word
+    if (bot.keyword === null) {
+      const random = Math.floor(Math.random() * categories.length);
+      bot.keyword = categories[random].split('_').join(' ');
+    }
+
+    const randomImg = Math.floor(Math.random() * 10);
+    const url = "https://api.unsplash.com/search/photos";
+
+    $.ajax({
+        url: url,
+        method: "GET",
+        data: {
+          query: bot.keyword,
+          per_page: 10,
+        },
+        headers: {
+          Authorization: "Client-ID " + APIKEY,
+        },
+      })
+      .done((data) => {
+        console.log(data);
+        var image = document.getElementById("img-pred");
+        image.src = data.results[randomImg].urls.small;
+
+        //save for presentation (img href type)
+        bot.img = data.results[randomImg].urls.small;
+        this.history.push(bot);
+
+        setTimeout(() => {
+          stateHandler();
+        }, 3000);
+      })
+      .fail((err) => {
+        throw err;
+      });
+  }
+
+  guessState() {
+    this.showPlayerState();
+
+    this.show("guess-state");
+  }
+
+  endState() {
+    console.log(this.history);
+
+    //TODO: 
+    // * LOOP THROUGH HISTORY TO PRESENT THEM.  
+
+    this.show("end-state");
+  }
+
+  show(id) {
+    // Show the chosen ID
+    this.stateID.forEach((state) => {
+      if (id === state)
+        document
+        .getElementById(state)
+        .style.setProperty("display", "block", "important");
+      else
+        document
+        .getElementById(state)
+        .style.setProperty("display", "none", "important");
+    });
+  }
+
+  restart() {
+    console.log("restart");
+
+    document
+      .getElementById("welcome-state")
+      .style.setProperty("display", "block", "important");
+    document
+      .getElementById("game-state")
+      .style.setProperty("display", "none", "important");
+    $("#players").empty();
+
+    document.getElementById("input-guess").value = '';
+
+    this.players = [];
+    this.playerIndex = 0;
+    this.state = 0;
+  }
 }
--- demo/telestrations.ai/main.js
+++ demo/telestrations.ai/main.js
@@ -3,38 +3,40 @@ var categories = [];
 var coords = [];
 var mousePressed = false;
 var model;
+var gm;
 const imgSize = 28;
 //const path = 'model1';
-const path = 'data/model4';
+const path = "data/model3";
+var players = [];
+const APIKEY = "y-bm_k6E6SHHkWoUXCcgPDrHRp-YhwbtwHRASagi9mM";
 
 $(() => {
   init();
-  canvas = new fabric.Canvas('canvas');
-  canvas.backgroundColor = '#ffffff';
+  canvas = new fabric.Canvas("canvas");
+  canvas.backgroundColor = "#ffffff";
   canvas.isDrawingMode = 0;
   canvas.freeDrawingBrush.color = "black";
   canvas.freeDrawingBrush.width = 10;
   canvas.renderAll();
 
   //listeners
-  canvas.on('mouse:up', function (e) {
+  canvas.on("mouse:up", function (e) {
     //getFrame();
     mousePressed = false;
   });
 
-  canvas.on('mouse:down', function (e) {
+  canvas.on("mouse:down", function (e) {
     mousePressed = true;
-
   });
 
-  canvas.on('mouse:move', function (e) {
+  canvas.on("mouse:move", function (e) {
     recordCoords(e);
   });
 });
 
 async function init() {
   // Load model
-  model = await tf.loadLayersModel(path + '/model.json');
+  model = await tf.loadLayersModel(path + "/model.json");
 
   // Test
   model.predict(tf.zeros([1, imgSize, imgSize, 1]));
@@ -58,7 +60,6 @@ function recordCoords(event) {
 
 function getFrame() {
   if (coords.length >= 2) {
-
     const imgData = getImageData();
 
     const pred = model.predict(preprocess(imgData)).dataSync();
@@ -72,7 +73,7 @@ function getFrame() {
     console.log(probs);
     console.log(names);
 
-    $('#pred').text(names[0].split('_').join(' ') + ' ' + probs[0].toFixed(3) * 100 + '% probability');
+    return names[0].split("_").join(" ");
   }
 }
 
@@ -87,11 +88,11 @@ function getCategories(indices) {
 }
 
 async function loadCategories() {
-  txt_path = path + '/class_names.txt';
+  txt_path = path + "/class_names.txt";
 
   await $.ajax({
     url: txt_path,
-    dataType: 'text'
+    dataType: "text",
   }).done((data) => {
     //If read successfully
     const categoriesList = data.split("\n");
@@ -112,7 +113,6 @@ function findIndicesOfMax(pred, maxPred) {
       }); // Sort in descending order;
       res.pop();
     }
-
   }
 
   return res;
@@ -132,7 +132,7 @@ function findTopValues(pred, maxPred) {
 
 //get the best bounding box by trimming around the drawing
 function getMinBoundingBox() {
-  //get x-coordinates 
+  //get x-coordinates
   var xCoor = coords.map((c) => {
     return c.x;
   });
@@ -145,18 +145,18 @@ function getMinBoundingBox() {
   //find min x and y coordinates
   var min_coords = {
     x: Math.min.apply(null, xCoor),
-    y: Math.min.apply(null, yCoor)
+    y: Math.min.apply(null, yCoor),
   };
 
   //find max x and y coordinates
   var max_coords = {
     x: Math.max.apply(null, xCoor),
-    y: Math.max.apply(null, yCoor)
+    y: Math.max.apply(null, yCoor),
   };
 
   return {
     min: min_coords,
-    max: max_coords
+    max: max_coords,
   };
 }
 
@@ -169,7 +169,12 @@ function getImageData() {
   const dpi = window.devicePixelRatio;
 
   // (x: x-axis coordinate of the top-left corner, y: y-axis coordinate of the top-left corner, z: width, w: height)
-  const imgData = canvas.contextContainer.getImageData(mbb.min.x * dpi, mbb.min.y * dpi, (mbb.max.x - mbb.min.x) * dpi, (mbb.max.y - mbb.min.y) * dpi);
+  const imgData = canvas.contextContainer.getImageData(
+    mbb.min.x * dpi,
+    mbb.min.y * dpi,
+    (mbb.max.x - mbb.min.x) * dpi,
+    (mbb.max.y - mbb.min.y) * dpi
+  );
 
   return imgData;
 }
@@ -177,29 +182,156 @@ function getImageData() {
 function preprocess(imgData) {
   return tf.tidy(() => {
     // Convert to tensor
-    let tensor = tf.browser.fromPixels(imgData, nrChannels = 1);
+    let tensor = tf.browser.fromPixels(imgData, (nrChannels = 1));
 
     // Resize
-    const resize = tf.image.resizeBilinear(tensor, [imgSize, imgSize]).toFloat();
-
+    const resize = tf.image
+      .resizeBilinear(tensor, [imgSize, imgSize])
+      .toFloat();
 
     // Normalize => 1.0 - (data/255.0)
     const offset = tf.scalar(255.0);
     const normalize = tf.scalar(1.0).sub(resize.div(offset));
 
-
     // Batch Shape
     const batch = normalize.expandDims(0);
-
 
     return batch;
   });
 }
 
+// Save canvas as image
+function saveCanvas() {
+  return canvas.toDataURL();
+}
+
 //Clear the canvas
 function clearCanvas() {
   canvas.clear();
-  canvas.backgroundColor = '#ffffff';
+  canvas.backgroundColor = "#ffffff";
   coords = [];
-  console.log('cleared');
+  console.log("cleared");
+}
+
+//add player to list and array
+function addPlayer(e) {
+  e.preventDefault();
+
+  // Getting box and ul by selecting id;
+  let input = document.getElementById("box");
+
+  if (
+    input.value != "" &&
+    !players.find((player) => player.name === input.value)
+  ) {
+    const newPlayer = new Player(input.value);
+    players.push(newPlayer);
+    // console.log(players);
+
+    if (players.length > 2) {
+      // console.log("more than 2 players")
+      $("#start-button").removeClass("disabled");
+      $("#start-button").addClass("enabled");
+      document.getElementById("start-button").disabled = false;
+    }
+
+    $("#players").prepend(
+      `<li id="${newPlayer.name}" class="list-group-item d-flex justify-content-between align-items-center">` +
+      input.value +
+      `<span class="badge badge-danger badge-pill" onclick="removePlayer(this)">X</span></li>`
+    );
+
+    input.value = "";
+  }
+}
+
+function removePlayer(obj) {
+  console.log(obj.parentNode.id);
+  var name = obj.parentNode.id;
+  $("#" + name).remove();
+
+  players = players.filter(function (player) {
+    return player.name != name;
+  });
+
+  if (players.length < 3) {
+    // console.log("less than 3 players")
+    $("#start-button").addClass("disabled");
+    $("#start-button").removeClass("enabled");
+    document.getElementById("start-button").disabled = true;
+  }
+}
+
+function stateHandler(e = null, restart = false) {
+  if (e !== null) e.preventDefault();
+
+  if (!gm) {
+    // Start our game
+    gm = new GameState([]);
+  }
+
+  // Open game state
+  if (gm.playerIndex === 0) {
+    hideAndShowElementsByID("welcome-state", "game-state");
+
+    // Add players to game state
+    gm.players = players;
+  }
+
+  if (gm.players.length < 2) return;
+
+  if (restart) {
+    console.log("Restart Game");
+
+    gm.restart();
+  }
+
+  // End state
+  if (gm.playerIndex === gm.players.length && gm.state !== 1) {
+    console.log("End state");
+    players = [];
+    gm.endState();
+    return;
+  }
+
+  // Draw state
+  if (gm.state === 0) {
+    console.log("Draw State", gm.players[gm.playerIndex]);
+
+    gm.drawState();
+
+    gm.playerIndex++;
+  }
+
+  // AI state
+  else if (gm.state === 1) {
+    console.log("AI State");
+
+    gm.AIState();
+  }
+
+  // Guess state
+  else if (gm.state === 2) {
+    console.log("Guess State", gm.players[gm.playerIndex]);
+
+    gm.guessState();
+
+    gm.playerIndex++;
+  }
+
+  gm.state++;
+  gm.state = gm.state % 3;
+}
+
+function hideAndShowElementsByID(hideID, showID) {
+  document
+    .getElementById(hideID)
+    .style.setProperty("display", "none", "important");
+  document
+    .getElementById(showID)
+    .style.setProperty("display", "block", "important");
+}
+
+function hide(el) {
+  el.parentNode.parentNode.style.setProperty("display", "none", "important");
 }
